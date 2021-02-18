3780e86276b3399dc9dbe9fe3761054cf7515b7d
Robert Hsu <roberthsu2003@gmail.com>
2021-02-18 15:49:14 +0800

    modify

--- 上課內容/sample1.py
+++ 上課內容/sample1.py
@@ -1,12 +1,12 @@
-from PERSON import Person
+from PERSON import Person as P
 
-someOne = Person('張xx',170,70)
+someOne = P('張xx',170,70)
 print(someOne.name)
 print(someOne.height)
 print(someOne.weight)
 print('bmi:',someOne.bmi())
 
-someTwo = Person('王xx',160,45)
+someTwo = P('王xx',160,45)
 print(someTwo.name)
 print(someTwo.height)
 print(someTwo.weight)
