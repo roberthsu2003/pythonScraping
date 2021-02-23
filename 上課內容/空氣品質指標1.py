718a183a7bb52e0e96620848bc90edca73645445
Robert Hsu <roberthsu2003@gmail.com>
2021-02-23 16:38:44 +0800

    modify

--- 上課內容/空氣品質指標1.py
+++ 上課內容/空氣品質指標1.py
@@ -8,27 +8,28 @@ from tkinter import *
 import requests
 
 class AirWindow(Tk):
+    url = "https://data.epa.gov.tw/api/v1/aqx_p_432?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json"
     def __init__(self):
         super().__init__()
-        btn = Button(self, text="取得目前AQI指數", padx=20, pady=20, command=buttonClick)
+        btn = Button(self, text="取得目前AQI指數", padx=20, pady=20, command=self.buttonClick)
         btn.pack()
 
-url = "https://data.epa.gov.tw/api/v1/aqx_p_432?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json"
-def buttonClick():
-    response = requests.get(url)
-    if response.status_code == 200:
-        print("下載成功")
-    else:
-        print("失敗")
-    allData = response.json()  # dictinary
-    records = allData['records']  # list
-    for record in records:  # record是dictionary
-        print("監測點:", record["SiteName"])
-        print("城市:", record['County'])
-        print("AQI:", record['AQI'])
-        print("狀態:", record['Status'])
-        print("時間:", record['ImportDate'])
-        print("========")
+    def buttonClick(self):
+        response = requests.get(AirWindow.url)
+        if response.status_code == 200:
+            print("下載成功")
+        else:
+            print("失敗")
+        allData = response.json()  # dictinary
+        records = allData['records']  # list
+        for record in records:  # record是dictionary
+            print("監測點:", record["SiteName"])
+            print("城市:", record['County'])
+            print("AQI:", record['AQI'])
+            print("狀態:", record['Status'])
+            print("時間:", record['ImportDate'])
+            print("========")
+
 
 
 
@@ -38,5 +39,4 @@ if __name__ == "__main__":
     window=AirWindow()
     window.title("空氣品質指標")
     window.geometry("400x150")
-
     window.mainloop()
