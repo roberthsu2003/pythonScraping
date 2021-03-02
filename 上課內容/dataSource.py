5a58e45ead1d116c0440fdcade8dcc0f72eded6c
Robert Hsu <roberthsu2003@gmail.com>
2021-03-02 16:47:40 +0800

    modify

--- 上課內容/dataSource.py
+++ 上課內容/dataSource.py
@@ -4,9 +4,6 @@ from requests import ConnectionError,HTTPError,Timeout
 url = "https://data.epa.gov.tw/api/v1/aqx_p_432?limit=1000&api_key=9be7b239-557b-4c10-9775-78cadfc555e9&format=json"
 newRecords = [] #建立要傳出的list
 
-def __import__():
-    print("import")
-
 def getAirData():
     try:
         response = requests.get(url)
