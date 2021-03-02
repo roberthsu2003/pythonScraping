aa627fead31f65bc1a84028cd5c40e787a63eca8
Robert Hsu <roberthsu2003@gmail.com>
2021-03-02 13:47:37 +0800

    modify

---
+++ 上課內容/dataSource.py
@@ -0,0 +1,2 @@
+def getAirData():
+    return None
--- 上課內容/空氣品質指標2.py
+++ 上課內容/空氣品質指標2.py
@@ -4,9 +4,14 @@
 '''
 
 from tkinter import Tk
+import dataSource
+
+
 class AirWindow(Tk):
     def __init__(self):
         super().__init__()
+        #取得資料
+        print(dataSource.getAirData())
         self.title("台灣各地空氣品質指標")
         self.geometry('300x100+200+200')
         self.resizable(width=0, height=0)
