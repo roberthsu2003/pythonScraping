cf84c58e4e525e5d5d77826a275a3d5ae391890d
Robert Hsu <roberthsu2003@gmail.com>
2021-03-09 14:54:54 +0800

    modify

--- 上課內容/台北市youbike/dataSource/draw.py
+++ 上課內容/台北市youbike/dataSource/draw.py
@@ -2,5 +2,6 @@ import tkinter as tk
 def getInfoCanvas(parent):
     infoCanvas=tk.Canvas(parent,width=200,height=100,bg='#ffffff')
     infoCanvas.create_oval(10,10,30,30,fill='red')
-    infoCanvas.create_oval(10,60,30,30,fill='orange')
+    infoCanvas.create_oval(10,60,30,40,fill='orange')
+    infoCanvas.create_oval(10,90, 30, 70, fill='green')
     return infoCanvas
