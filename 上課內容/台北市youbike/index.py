5ebc4aeb8c87ab6c1fb55f5496fdecd9e0c1b805
Robert Hsu <roberthsu2003@gmail.com>
2021-03-09 13:46:12 +0800

    modify

--- 上課內容/台北市youbike/index.py
+++ 上課內容/台北市youbike/index.py
@@ -5,14 +5,19 @@ import tkinter.ttk as ttk
 class YoubikeWindow(tk.Tk):
     def __init__(self):
         super().__init__()
+        self.title("台北市youbike及時資訊")
+        mediumFont = {'font':('Arial', 20)}
+
 
         #display interface
-        leftFrame = ttk.LabelFrame(self,text='台北市行政區')
-        areaList = tk.Listbox(leftFrame)
+        leftFrame = ttk.LabelFrame(self,text='台北市行政區',style='LLabelFrame')
+        areaList = tk.Listbox(leftFrame,**mediumFont)
         for name in areas:
             areaList.insert(tk.END,name)
         areaList.pack(padx=10,pady=10)
         leftFrame.pack(side=tk.LEFT,padx=30,pady=30)
+        style = ttk.Style(self)
+        style.configure('LLabelFrame', font=('Arial', 20))
 
 
 if __name__ == "__main__":
