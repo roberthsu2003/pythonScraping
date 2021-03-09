d5e0ef209258e51943b54ab2766de7361ae8c19c
Robert Hsu <roberthsu2003@gmail.com>
2021-03-09 11:40:56 +0800

    modify

--- 上課內容/台北市youbike/index.py
+++ 上課內容/台北市youbike/index.py
@@ -6,6 +6,14 @@ class YoubikeWindow(tk.Tk):
     def __init__(self):
         super().__init__()
 
+        #display interface
+        leftFrame = ttk.LabelFrame(self,text='台北市行政區')
+        areaList = tk.Listbox(leftFrame)
+        for name in areas:
+            areaList.insert(tk.END,name)
+        areaList.pack(padx=10,pady=10)
+        leftFrame.pack(side=tk.LEFT,padx=30,pady=30)
+
 
 if __name__ == "__main__":
     window = YoubikeWindow()
