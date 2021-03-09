ae315f1a349fe4d9607f7058ca79e42feca37ac1
Robert Hsu <roberthsu2003@gmail.com>
2021-03-09 15:07:59 +0800

    modify

--- 上課內容/台北市youbike/dataSource/draw.py
+++ 上課內容/台北市youbike/dataSource/draw.py
@@ -2,6 +2,9 @@ import tkinter as tk
 def getInfoCanvas(parent):
     infoCanvas=tk.Canvas(parent,width=200,height=100,bg='#ffffff')
     infoCanvas.create_oval(10,10,30,30,fill='red')
-    infoCanvas.create_oval(10,60,30,40,fill='orange')
-    infoCanvas.create_oval(10,90, 30, 70, fill='green')
+    infoCanvas.create_text(70,20,text='已無車',font=('Arial',13,'bold'))
+    infoCanvas.create_oval(10,40,30,60,fill='orange')
+    infoCanvas.create_text(70, 50, text='快沒車', font=('Arial', 13, 'bold'))
+    infoCanvas.create_oval(10,70, 30,90, fill='green')
+    infoCanvas.create_text(70, 80, text='車很多', font=('Arial', 13, 'bold'))
     return infoCanvas
